53917f00d40345164090668b33ae1e8969d374e5
MArkLesterAgaser <MArkLesterAgaser@SMU-0034>
2025-11-27 17:44:27 +0800

    Add project files.

---
+++ Labitoria.JD.Winforms.UserManager/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+
+namespace Labitoria.JD.Winforms.UserManager
+{
+    public class UserRepository
+    {
+        private readonly ISQLiteConnection _connection;
+
+        public UserRepository()
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserData.db");
+        _connection = new SQLiteConnection(databasePath);
+            _connection.CreateTable<User>();
+        
+        }
+        public bool Add(User user)
+        {
+            _connection.Insert(user);
+            return true;
+        }
+
+        public List<User> GetAll() 
+        { 
+            return new List<User>();
+        }
+
+        public User Get(int id) 
+        {
+            return new User("DummyFullname","DummyUsername","DummyPassword");
+        }
+    }
+}
